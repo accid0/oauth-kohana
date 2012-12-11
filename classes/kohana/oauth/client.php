8118fe9cf87fc1067a2cf80006735e0ec2f629e0
reHePaJI <kontakt.asch@gmail.com>
2012-12-11 22:49:13 +0200

    inherit

--- classes/kohana/oauth/client.php
+++ classes/kohana/oauth/client.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 /*
  * oauth_client.php
  *
---
+++ classes/oauth.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Адмын
+ * Date: 11.12.12
+ * Time: 22:41
+ * To change this template use File | Settings | File Templates.
+ */
+class Oauth extends Kohana_Oauth_Client
+{
+
+}
